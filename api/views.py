ce77f35990c55d9b4acbf7fa9f1ec03beb3421f2
Hye-S <gptjs7970@naver.com>
2022-08-24 17:30:12 +0900

    Add: 검색 기능 구현

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 # django rest api
 from rest_framework import viewsets
-
+from rest_framework.filters import SearchFilter
 # local modules
 from .models import Post
 from .serializer import PostSerializer, PostDetailSerializer, PostCreateSerializer
@@ -12,6 +12,9 @@ class PostViewSet(viewsets.ModelViewSet):
     """
     queryset = Post.objects.select_related('company').all()
     serializer_class = PostSerializer
+    # 검색 기능
+    filter_backends = [SearchFilter]
+    search_fields = ['position', 'skill', 'description']
 
     def get_serializer_class(self):
         if self.action == "create":
